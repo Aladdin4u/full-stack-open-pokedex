63467dc2ac65738c6c623ecbc2c15f00ac90f63d
aladdin4u <taofikakinkumi@gmail.com>
2022-12-26 20:31:31 +0100

    remove unnecessary console.log

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    browser: true,
+    node: true,
     es6: true,
     "jest/globals": true,
   },
@@ -15,7 +15,7 @@ module.exports = {
   plugins: ["react", "jest"],
   rules: {
     indent: ["error", 2],
-    "linebreak-style": ["error", "uwindows"],
+    "linebreak-style": ["error", "windows"],
     quotes: ["error", "single"],
     semi: ["error", "never"],
     eqeqeq: "error",
